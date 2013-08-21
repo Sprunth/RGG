f3bb9de2a786a2a42db59388060b6fdd3fb326d2
Kyle Lutz <kyle.lutz@kitware.com>
2013-08-21 10:19:06 -0400

    Add caps to layered pin cells

--- Application/vtkCmbLayeredConeSource.cxx
+++ Application/vtkCmbLayeredConeSource.cxx
@@ -6,6 +6,16 @@
 #include <vtkInformation.h>
 #include <vtkInformationVector.h>
 
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkNew.h>
+#include <vtkMath.h>
+#include <vtkAppendPolyData.h>
+#include <vtkDoubleArray.h>
+
 #include "vtkCmbConeSource.h"
 
 vtkStandardNewMacro(vtkCmbLayeredConeSource);
@@ -83,16 +93,113 @@ int vtkCmbLayeredConeSource::RequestData(
   for(int i = 0; i < this->GetNumberOfLayers(); i++)
     {
     vtkCmbConeSource *cone = vtkCmbConeSource::New();
-    cone->SetCapping(i == 0);
     cone->SetBaseRadius(this->GetBaseRadius(i));
     cone->SetTopRadius(this->GetTopRadius(i));
     cone->SetHeight(this->Height);
     cone->SetBaseCenter(this->BaseCenter);
     cone->SetResolution(this->Resolution);
     cone->SetDirection(this->Direction);
-    cone->Update();
-    output->SetBlock(i, cone->GetOutput());
-    cone->Delete();
+
+    if(i == 0)
+      {
+      cone->SetCapping(true);
+      cone->Update();
+      output->SetBlock(i, cone->GetOutput());
+      cone->Delete();
+      }
+    else
+      {
+      // create cap for layer
+      double angle = 2.0*3.141592654/this->Resolution;
+
+      vtkAppendPolyData *merger = vtkAppendPolyData::New();
+      cone->SetCapping(false);
+      cone->Update();
+      merger->AddInputData(cone->GetOutput());
+      cone->Delete();
+
+      double heights[] = { 0, this->Height };
+      for(int height = 0; height < 2; height++)
+        {
+        double h = heights[height];
+        double r1 = height == 0 ? this->GetBaseRadius(i-1) : this->GetTopRadius(i-1);
+        double r2 = height == 0 ? this->GetBaseRadius(i) : this->GetTopRadius(i);
+
+        vtkPolyData *cap = vtkPolyData::New();
+        cap->Allocate();
+        vtkPoints *points = vtkPoints::New();
+        vtkCellArray *cells = vtkCellArray::New();
+
+        // add base cap points
+        double point[3];
+        for(int j = 0; j < this->Resolution; j++)
+          {
+          point[0] = h;
+          point[1] = r1 * cos(j * angle);
+          point[2] = r1 * sin(j * angle);
+          points->InsertNextPoint(point);
+          }
+        for(int j = 0; j < this->Resolution; j++)
+          {
+          point[0] = h;
+          point[1] = r2 * cos(j * angle);
+          point[2] = r2 * sin(j * angle);
+          points->InsertNextPoint(point);
+          }
+
+        // add cells
+        for(int j = 0; j < this->Resolution; j++)
+          {
+          int res = this->Resolution;
+          vtkIdType cell[] = { j, (j + 1) % res, (j + 1) % res + res, j + res };
+          cells->InsertNextCell(4, cell);
+          }
+
+        cap->SetPoints(points);
+        cap->SetPolys(cells);
+        points->Delete();
+        cells->Delete();
+
+        // A non-default origin and/or direction requires transformation
+        if ( this->BaseCenter[0] != 0.0 || this->BaseCenter[1] != 0.0 ||
+             this->BaseCenter[2] != 0.0 || this->Direction[0] != 1.0 ||
+             this->Direction[1] != 0.0 || this->Direction[2] != 0.0 )
+          {
+          vtkTransform *t = vtkTransform::New();
+          t->Translate(this->BaseCenter[0], this->BaseCenter[1], this->BaseCenter[2]);
+          double vMag = vtkMath::Norm(this->Direction);
+          if ( this->Direction[0] < 0.0 )
+            {
+            // flip x -> -x to avoid instability
+            t->RotateWXYZ(180.0, (this->Direction[0]-vMag)/2.0,
+                          this->Direction[1]/2.0, this->Direction[2]/2.0);
+            t->RotateWXYZ(180.0, 0, 1, 0);
+            }
+          else
+            {
+            t->RotateWXYZ(180.0, (this->Direction[0]+vMag)/2.0,
+                          this->Direction[1]/2.0, this->Direction[2]/2.0);
+            }
+          vtkNew<vtkTransformPolyDataFilter> filter;
+          filter->SetTransform(t);
+          t->Delete();
+          filter->SetInputDataObject(cap);
+          cap->Delete();
+          filter->Update();
+          merger->AddInputData(filter->GetOutput());
+          }
+        else
+          {
+          merger->AddInputData(cap);
+          cap->Delete();
+          }
+        }
+
+      // merge all polydata
+      merger->Update();
+      output->SetBlock(i, merger->GetOutput());
+      merger->Delete();
+      }
     }
 
   return 1;
