fde087de5b26625adb92b63987719ebac12e6cd0
Jacob Becker <jacob.becker@kitware.com>
2014-04-09 20:49:48 -0400

    added core clear

--- Application/cmbNucMainWindow.cxx
+++ Application/cmbNucMainWindow.cxx
@@ -285,7 +285,7 @@ void cmbNucMainWindow::initPanels()
     SIGNAL(materialVisibilityChanged(const QString&)), this,
     SLOT(onObjectModified()));
   QObject::connect(this->InputsWidget, SIGNAL(deleteCore()),
-                   this, SLOT(clearAll()));
+                   this, SLOT(clearCore()));
 }
 
 void cmbNucMainWindow::onObjectSelected(AssyPartObj* selObj,
@@ -623,6 +623,18 @@ void cmbNucMainWindow::clearAll()
   this->ui->qvtkWidget->update();
   this->Renderer->ResetCamera();
   this->Renderer->Render();
+}
+
+void cmbNucMainWindow::clearCore()
+{
+  if(this->NuclearCore)
+  {
+    this->NuclearCore->clearExceptAssembliesAndGeom();
+    this->InputsWidget->updateUI(true);
+    this->ui->qvtkWidget->update();
+    this->Renderer->ResetCamera();
+    this->Renderer->Render();
+  }
 }
 
 void cmbNucMainWindow::updatePinCellMaterialColors(PinCell* pin)
