18820e229c5416772f752046e6afc94949908fa4
Jacob Becker <jacob.becker@kitware.com>
2014-08-12 14:13:53 -0400

    attempt to make netcdf more relyable

--- meshkit/Projects/netcdf.cmake
+++ meshkit/Projects/netcdf.cmake
@@ -1,32 +1,55 @@
-get_libraries(hdf5 hdf5_libraries)
-get_filename_component(hdf5_path ${hdf5_libraries} PATH)
-get_filename_component(hdf5_path "${hdf5_path}../" PATH)
 
-if(APPLE)
 
-  add_external_project(netcdf
-    DEPENDS hdf5
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/Projects/patches/load.c <SOURCE_DIR>/ncgen3/load.c
-    USE_AUTOCONF
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND  <SOURCE_DIR>/configure
-    --enable-netcdf4
-    --prefix=<INSTALL_DIR>
-    "CPPFLAGS=-I${hdf5_path}/include ${cppflags}"
-    "LDFLAGS=-L${hdf5_path}/lib -arch ${CMAKE_OSX_ARCHITECTURES}" )
+#this will only modify the cppflags for netcdf as the next project in the tree
+#moab will unset the cppflags back to the original values
+if (build-projects)
+  set (pre_netcdf_cpp_flags ${cppflags})
+  set (pre_netcdf_c_flags ${cflags})
 
-else()
-
-  add_external_project(netcdf
-    DEPENDS hdf5
-    USE_AUTOCONF
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND CPPFLAGS=-I${hdf5_path}/include LDFLAGS=-L${hdf5_path}/lib <SOURCE_DIR>/configure
-    --enable-netcdf4
-    --prefix=<INSTALL_DIR> )
-
+  set (cppflags "-I${install_location}/include ${cppflags}")
+  set (cflags "-I${install_location}/include ${cflags}")
 endif()
+
+add_external_project(netcdf
+  DEPENDS hdf5
+
+  PATCH_COMMAND
+    # this patch fixes following issues:
+    # 1. incorrect configure target
+    # 2. properly use the provided szip find package.
+    ${CMAKE_COMMAND} -E copy_if_different  ${CMAKE_SOURCE_DIR}/Projects/patches/netcdf.CMakeLists.txt
+                                          <SOURCE_DIR>/CMakeLists.txt
+
+  CMAKE_ARGS
+    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DBUILD_UTILITIES:BOOL=ON
+    -DENABLE_NETCDF:BOOL=ON
+    -DUSE_SZIP:BOOL=ON
+    -DUSE_HDF5:BOOL=ON
+    -DENABLE_NETCDF_4:BOOL=ON
+    -DSZIP_INCLUDE_DIR:FILEPATH=${install_location}/include
+)
+
+add_external_project_step(patch_fix_ncgen3
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different
+           ${CMAKE_SOURCE_DIR}/Projects/patches/netcdf.ncgen3.load.c
+          <SOURCE_DIR>/ncgen3/load.c
+  DEPENDEES update # do after update
+  DEPENDERS patch  # do before patch
+  )
 
 
 ExternalProject_Get_Property(${name} install_dir)
 set_libraries(netcdf ${install_dir}/lib/libnetcdf${CMAKE_SHARED_LIBRARY_SUFFIX})
+
+if(APPLE)
+#special mac only script to fixup boost plugin install-names so that developers
+#can use the superbuild properly from other projects
+add_external_project_step(install_name_fixup
+  COMMENT "Fixing netcdf library install_names."
+  COMMAND  ${CMAKE_COMMAND}
+    -Dinstall_location:PATH=${install_location}
+    -Dlib_name:STRING="netcdf"
+    -P ${CMAKE_CURRENT_LIST_DIR}/apple/fixup_library_rpath.cmake
+  DEPENDEES install)
+endif()
