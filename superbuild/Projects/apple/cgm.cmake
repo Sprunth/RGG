bc718fe13a6b057b364d67dd442f3ca86a26979f
Jacob Becker <jacob.becker@kitware.com>
2014-10-21 18:26:18 -0400

    suppress cgm output on apple

--- superbuild/Projects/apple/cgm.cmake
+++ superbuild/Projects/apple/cgm.cmake
@@ -1,7 +1,18 @@
 #cgm has to be built in source to work
+
+option(SUPPRESS_CGM_BUILD_OUTPUT
+"Suppress CGM build output"
+ON)
+mark_as_advanced(SUPPRESS_CGM_BUILD_OUTPUT)
+
+if(SUPPRESS_CGM_BUILD_OUTPUT)
+set(suppress_build_out SUPPRESS_BUILD_OUTPUT)
+endif()
+
 add_external_project(cgm
   DEPENDS OCE
   BUILD_IN_SOURCE 1
+  ${suppress_build_out}
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
     --with-occ=${OCE_DIR}
     --prefix=<INSTALL_DIR>
