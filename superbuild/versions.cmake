b99580f4c23891244d7eedc30003206fe79889d1
Jacob Becker <jacob.becker@kitware.com>
2014-11-18 20:29:37 -0500

    update to use master of meshkit

--- superbuild/versions.cmake
+++ superbuild/versions.cmake
@@ -154,8 +154,9 @@ else()
 endif()
 
 add_revision(remus
-  GIT_REPOSITORY http://public.kitware.com/Remus.git
-  GIT_TAG 7f9fbb25f78e0b7627e31c0d55d28716bb75b3e4
+  GIT_REPOSITORY https://github.com/robertmaynard/Remus.git
+  #GIT_REPOSITORY http://public.kitware.com/Remus.git
+  GIT_TAG c21bfabd468a5b2f99f91184b643b5fea2645aea
   )
 
 add_revision(kml
@@ -184,7 +185,7 @@ add_revision(cgm
 
 add_revision(lasso
   GIT_REPOSITORY https://bitbucket.org/fathomteam/lasso.git
-  GIT_TAG Version3.1
+  GIT_TAG master
   )
 
 if(BUILD_WITH_CUBIT AND UNIX AND NOT APPLE)
@@ -216,13 +217,13 @@ add_revision(netcdfcpp
   URL_MD5 d32b20c00f144ae6565d9e98d9f6204c
 )
 
-if(WIN32)
-add_revision(moab
-  GIT_REPOSITORY https://judajake@bitbucket.org/judajake/moab.git
-  GIT_TAG 4.6.3_cmake)
-else()
+#if(WIN32)
+#add_revision(moab
+#  GIT_REPOSITORY https://judajake@bitbucket.org/judajake/moab.git
+#  GIT_TAG 4.6.3_cmake)
+#else()
 add_revision(moab
   GIT_REPOSITORY https://bitbucket.org/fathomteam/moab.git
-  GIT_TAG Version4.7.0
+  GIT_TAG vijaysm/cmake-distcheck
   )
-endif()
+#endif()
