4725cbd9fa5f7af5205b577e1c7d0079a8518178
Jacob Becker <jacob.becker@kitware.com>
2014-11-21 16:39:19 -0500

    disable dap on windows because no curl

---
+++ superbuild/Projects/win32/netcdf.cmake
@@ -0,0 +1,38 @@
+option(SUPPRESS_NETCDF_BUILD_OUTPUT
+"Suppress netcdf build output"
+ON)
+mark_as_advanced(SUPPRESS_NETCDF_BUILD_OUTPUT)
+
+set(suppress_build_out)
+
+if(SUPPRESS_NETCDF_BUILD_OUTPUT)
+set(suppress_build_out SUPPRESS_BUILD_OUTPUT)
+endif()
+
+#this will only modify the cppflags for netcdf as the next project in the tree
+#moab will unset the cppflags back to the original values
+if (build-projects)
+  set (pre_netcdf_cpp_flags ${cppflags})
+  set (cppflags "-I${install_location}/include ${cppflags}")
+endif()
+
+add_external_project(netcdf
+  DEPENDS hdf5
+
+  PATCH_COMMAND
+    # this patch fixes following issues:
+    # 1. incorrect configure target
+    # 2. properly use the provided szip find package.
+    ${CMAKE_COMMAND} -E copy_if_different ${SuperBuild_PROJECTS_DIR}/patches/netcdf.src.CMakeLists.txt
+                                          <SOURCE_DIR>/CMakeLists.txt
+
+  CMAKE_ARGS
+    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DBUILD_UTILITIES:BOOL=ON
+    -DENABLE_NETCDF:BOOL=ON
+    -DUSE_SZIP:BOOL=ON
+    -DUSE_HDF5:BOOL=ON
+    -DENABLE_NETCDF_4:BOOL=ON
+    -DENABLE_DAP:BOOL=OFF
+  ${suppress_build_out}
+)
