558afc387e9a105bcf99b037faa2feb832aaa9a4
Jacob Becker <jacob.becker@kitware.com>
2014-12-22 14:09:35 -0500

    update the version number

--- superbuild/Projects/nuclearRGG.bundle.common.cmake
+++ superbuild/Projects/nuclearRGG.bundle.common.cmake
@@ -3,9 +3,9 @@
 # We hardcode the version numbers since we cannot determine versions during
 # configure stage.
 set (rgg_version_major 1)
-set (rgg_version_minor 1)
+set (rgg_version_minor 2)
 set (rgg_version_patch 0)
-set (rgg_version_suffix)
+set (rgg_version_suffix "Devel")
 set (rgg_version "${rgg_version_major}.${rgg_version_minor}")
 
 include (paraview_version)
