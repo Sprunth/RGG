1f5e5926dd51559a7ce275f579b9b0636eb1d75f
Ben Boeckel <ben.boeckel@kitware.com>
2015-01-21 16:53:33 -0500

    meshkit: tell ftgl where to find our freetype

--- meshkit/Projects/ftgl.cmake
+++ meshkit/Projects/ftgl.cmake
@@ -3,4 +3,6 @@ add_external_project(ftgl
   DEPENDS freetype
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS=ON
+    -DFREETYPE_INCLUDE_DIR_freetype2=<INSTALL_DIR>/include/freetype2
+    -DFREETYPE_INCLUDE_DIR_ft2build=<INSTALL_DIR>/include
   )
