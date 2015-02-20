5f2c8ec865d4979c0fa4edb7b178fb7f83f449f2
Ben Boeckel <ben.boeckel@kitware.com>
2015-02-20 12:53:30 -0500

    cgm: set the OCC usage variable

--- meshkit/Projects/cgm.cmake
+++ meshkit/Projects/cgm.cmake
@@ -2,16 +2,17 @@ set(extra_deps)
 if (BUILD_WITH_CUBIT)
   list(APPEND extra_deps
     cubit)
+  set(use_occ OFF)
 else ()
   list(APPEND extra_deps
     OCE)
+  set(use_occ ON)
 endif ()
 
 add_external_project(cgm
   DEPENDS ${extra_deps}
   CMAKE_ARGS
-    ${cgm_cmake_args}
-    -DCGM_KCM:BOOL=OFF
+    -DCGM_OCC:BOOL=${use_occ}
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
     -DUSE_MPI:BOOL=${BUILD_WITH_MPI})
 
