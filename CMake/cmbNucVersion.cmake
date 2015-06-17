f927ccc2a3a2a42ef698dccd1ba90ca69fe2056f
Jacob Becker <jacob.becker@kitware.com>
2015-06-17 16:35:22 -0400

    add creation tests

--- CMake/cmbNucVersion.cmake
+++ CMake/cmbNucVersion.cmake
@@ -1,5 +1,5 @@
 set (rgg_version_major 1)
 set (rgg_version_minor 2)
 set (rgg_version_patch 0)
-set (rgg_version_suffix "Devel")
+set (rgg_version_suffix "")
 set (rgg_version "${rgg_version_major}.${rgg_version_minor}")
